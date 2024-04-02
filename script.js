4a0c67e9a0d6d4701b56b9b7634003f89b1b2c9a
golibov <golibovshg7@gmail.com>
2024-04-02 13:20:37 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,77 @@
+// 1
+// let k = +prompt("Kerakli sonni kiriting: ");
+// let n = +prompt("Nechi marta chiqarish kerak ?");
+// for(let i=1; i<=n; i++){
+//     console.log(k)
+// }
+// console.log("Marxamat yaxshi kunlaga buyursin)");
+
+
+// 2
+// let a = +prompt("a ni kiritng:");
+// let b = +prompt("b ni kiritng:");
+// let i = 0;
+// for(i ; a<=b; i++){  
+//     console.log(a);  
+//     a++;
+// }
+// console.log(i + " ta son bor");
+
+
+// 3
+// let a = +prompt("a ni kiritng:");
+// let b = +prompt("b ni kiritng:");
+// for(let i=b - 1; i > a; i--){ 
+//     console.log(i)
+// }
+
+
+// 4
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=1; i<=10; i++){
+//     console.log(`${i} kg konfet ${i*sum} so'm`)
+// }
+
+
+// 5
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=1; i<=10; i++){
+//     console.log(`${i/10} kg konfet ${i/10*sum} so'm`)
+// }
+
+
+// 6
+// let sum=+prompt("Konfet narxini kiriting: ");
+// for(let i=10; i<=20; i++){
+//     console.log(`${i/10} kg konfet ${i/10*sum} so'm`)
+// }
+
+
+//7
+// let a = +prompt("a ni kiriting: ")
+// let b = +prompt("b ni kiriting: ")
+// let sum = 0;
+// for(let i=a+1; b>i; i++){
+//     sum+=i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
+
+
+// 8
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let sum = 1;
+// for(let i=a; b>i; i++){
+//     sum*=i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
+
+  
+// 9
+// let a = +prompt("a ni kiriting: ");
+// let b = +prompt("b ni kiriting: ");
+// let sum = 0;
+// for(let i=0; b>i; i++){
+//     sum = sum + i*i;
+// }
+// console.log(`${a} dan ${b} gacha raqamlar yigindisi = ${sum}`)
